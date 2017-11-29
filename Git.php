40fa25059068cc9308206f56e876b094ec1652d2
Ramón García <ramon@dred.com>
2017-11-29 12:07:02 +0100

    Update Git.php

--- Git.php
+++ Git.php
@@ -409,8 +409,9 @@ class GitRepo {
 	 * @param   boolean  should all files be committed automatically (-a flag)
 	 * @return  string
 	 */
-	public function commit($message = "", $commit_all = true) {
+	public function commit($message = "", $commit_all = true, $other_flags) {
 		$flags = $commit_all ? '-av' : '-v';
+		$flags .= ' ' . $other_flags;
 		return $this->run("commit ".$flags." -m ".escapeshellarg($message));
 	}
 
